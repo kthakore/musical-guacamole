fb92427c829059e190489908bd739a1fb3145341
Kartik Thakore <thakore.kartik@gmail.com>
2016-09-26 22:20:48 -0400

    More seamless testing of Kafka consumer

--- test/test-server.js
+++ test/test-server.js
@@ -9,29 +9,53 @@ var app       = require('../server'),
   fs        = require('fs'),
   Sequelize = require('sequelize'),
   sequelize = new Sequelize('magical_guac', 'postgres', 'GH4i9a91m', {'dialect' : 'postgres'}),
-  spawn = require('cross-spawn')
+  spawn = require('cross-spawn-with-kill')
 
-// Spawn NPM asynchronously
-var child = spawn('npm', ['run', 'start-consumer'], { stdio: 'inherit' })
 const logFile = 'app.log'
 
-
+/*
+ * Utility function to get the Last
+ * line of the app.log
+ */
 function GetLastLogLine() {
   var data  = fs.readFileSync(logFile, 'utf8'),
     lines = data.split('\n')
+  console.log(lines)
   // Since a new line character is added at the end of each log item
   // the last line will always be empty so get the line before that
   return lines[lines.length-2]
 }
 
+
+
 describe('App Server', function() {
 
+  // Varible to hold the Kafka Consumer
+  // process reference so it can be
+  // started before the tests run
+  // and killed after the tests finish
+  let childConsumerProcess
+
+  //TODO: Use some more yeild or promises to make
+  // this cleaner.
   before(function(done) {
     // Empty the log file so we have a clear start
     fs.truncate(logFile, 0, () => {
       // Empty the Users table
       sequelize.query('DELETE FROM users')
-      .then(() => {done()})
+      .then(() => {
+
+        /* For testing purposes start the file
+         * consume that will write to app.log
+         * Once started, sleeping for a second
+         * to ensure that the consumer is read.
+         * TODO: Use a real test to see if the
+         * consumer has actually started.
+         */
+        childConsumerProcess = spawn('npm', ['run', 'start-consumer'], { stdio: 'inherit' })
+
+        setTimeout(done, 1500)
+      })
     })
   })
 
@@ -46,10 +70,8 @@ describe('App Server', function() {
     .end(() => {
       setTimeout(() => {
         var line = GetLastLogLine()
-        // console.log('LINE IS: ', line)
-        line.should.match(JSON.stringify(message))
+        line.should.match('log ' + '0 ' + JSON.stringify(message))
         should.exist(line)
-        // Look for app.log to have file
 
         done()
       }, 500)
@@ -61,11 +83,13 @@ describe('App Server', function() {
   it('should allow POST on /classes/user', function(done) {
 
     let rand = Math.random()
+    let user = {email : 'foo' + rand + '@bar.com', password : 'abc123', name : 'foo' + rand}
     request.post('/classes/user')
-    .send({'email' : 'foo' + rand + '@bar.com', 'password' : 'abc123', 'name' : 'foo' + rand})
+    .send(user)
     .expect(200)
     .end(function testPOSTUserCall () {
 
+      // User be in the DB
       sequelize.query('SELECT * FROM users', { type: sequelize.QueryTypes.SELECT})
       .then(function(users) {
         // We don't need spread here, since only the results will be returned for select queries
@@ -73,14 +97,24 @@ describe('App Server', function() {
         should.exist(users[0])
         should.equal(users[0].email, 'foo' + rand + '@bar.com')
 
-        var line = GetLastLogLine()
+        // Time out to ensure that the app.log has been updated
+        setTimeout(() => {
 
-        should.exist(line)
-        // Look for app.log to have file
+          let line = GetLastLogLine()
+          should.exist(line)
+          let message = {actionId: 'USER_SIGNUP', data: user};
 
-        done()
+          line.should.match('log ' + '0 ' + JSON.stringify(message))
+          // Look for app.log to have file
+
+          done()
+
+        }, 700)
+
       })
+
     })
+
   })
 
 
@@ -103,8 +137,10 @@ describe('App Server', function() {
   })
 
   after(function(done) {
-    child.exit()
-    done()
+    // Click the consumer we started ealier
+    childConsumerProcess.kill()
+    // wait for the consumer process to be killed
+    setTimeout(done, 1000)
   })
 
 })
